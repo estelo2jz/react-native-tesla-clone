287c4114d0a5b9547dc9a5452d535da351a5cca0
esteloabell <manitobaaa@gmail.com>
2021-05-18 22:33:05 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,16 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import CarItem from './components/CarItem';
+import CarList from './components/CarList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem 
-        name={"Model 3"}
+      {/* <CarItem 
+        name={"Model X"}
         tagline={"Order Online For"}
         taglineCTA={"Touchless Delivery"}
         image={require("./assets/images/Model3.jpeg")}
-      />
+      /> */}
+      <Header />
+      <CarList />
+
       {/* View similar to div */}
       <StatusBar style="auto" />
     </View>
